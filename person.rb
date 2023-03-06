5e5ce18c058a3904dbaf10ab9769e51cbb2a80f2
Glenda Diaz <glendadiazah@outlook.com>
2023-03-06 15:48:28 -0600

    Fix linters

--- person.rb
+++ person.rb
@@ -1,56 +1,43 @@
 class Person
+  # Constructor
+  def initialize(id:, age:, name: 'Unknown', parent_permission: true)
+    # instance variables
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    # Constructor
-    def initialize(id, age, name='Unknown', parent_permission= true)
-        # instance variables
-        @id = id
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  # Get method
+  attr_reader :id
 
-    # Get method
-    def id
-        @id
-    end
+  # Get method
+  attr_reader :name
 
-    # Get method
-    def name
-        @name
-    end
+  # Get method
+  attr_reader :age
 
-    # Get method
-    def age
-        @age
-    end
+  # Set method
+  def name=(_name)
+    @name
+  end
 
-    # Set method
-    def name=(name)
-        @name
-    end
+  # Set method
+  def age=(_age)
+    @age
+  end
 
-    # Set method
-    def age=(age)
-        @age
-    end
+  private
 
-    private
-    def is_of_age?
-        unless @age>18
-            result = false
-        else
-            result = true
-        end
-        result
-    end
+  # rubocop:disable Naming/PredicateName
+  def is_of_age?
+    @age > 18
+  end
+  # rubocop:enable Naming/PredicateName
 
-    public
-    def can_use_services?
-        unless @parent_permission===true || @age>18
-            result = false
-        else
-            result = true
-        end
-        result
-    end
+  public
+
+  def can_use_services?
+    @parent_permission == true || @age > 18
+  end
 end
--- student.rb
+++ student.rb
@@ -1,11 +1,11 @@
 require_relative './person'
 class Student < Person
-    def initialize(classroom)
-        @classroom
-    end
+  def initialize(id:, age:, classroom:, name: 'Unknow', parent_permission: true)
+    super(id: id, age: age, name: name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        result = '¯\(ツ)/¯'
-        result
-    end
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
 end
