ab4c5df035b476f7040963e0c18610f80263ba05
Glenda Diaz <glendadiazah@outlook.com>
2023-03-15 14:43:29 -0600

    Add components test to person test and fix linters

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,25 +1,36 @@
 require_relative '../person'
 
 describe Person do
-    before :each do
-        @id = 01
-        @name = 'Test Name'
-        @age = 25
-      @person = Person.new(id: @id, name: @name, age: @age, parent_permission: true)
-    end
-  
-    describe '#new' do
-      it 'returns a new object' do
-        @person.should be_an_instance_of Person
-      end
-
-      it 'test method can_use_service?' do
-        expect(@person.can_use_services?).to be true
-      end
-
-      it 'test method correct_name' do
-        expect(@person.correct_name).to eq @name
-      end
-    end
-    
+  before :each do
+    @id = 0o1
+    @name = 'Test Name'
+    @age = 25
+    @person = Person.new(id: @id, name: @name, age: @age, parent_permission: true)
   end
+
+  describe '#new' do
+    it 'returns a new object' do
+      @person.should be_an_instance_of Person
+    end
+
+    it 'test id' do
+      expect(@person.id).to eq @id
+    end
+
+    it 'test name' do
+      expect(@person.name).to eq @name
+    end
+
+    it 'test age' do
+      expect(@person.age).to eq @age
+    end
+
+    it 'test method can_use_service?' do
+      expect(@person.can_use_services?).to be true
+    end
+
+    it 'test method correct_name' do
+      expect(@person.correct_name).to eq @name
+    end
+  end
+end
